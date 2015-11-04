c35e1cff3df2e81c1c1f8f6a0b946e992e0c3e80
kollstrom <e.kollstrom@gmail.com>
2015-11-04 12:56:07 +0100

    La til klokkeslett på kamper.txt, iframe på kamper.html, laster inn data og sammenligner dates i resultater.js

--- scripts/resultater.js
+++ scripts/resultater.js
@@ -1,15 +1,44 @@
 /**
  * Created by Emil on 03.11.2015.
+ *
+ * Linje 11: Funksjonen lastInnfil() laster inn en fil fra en iframe som ligger i kamper.html
+ * Linje 14: Splitter filen opp i liste av kamper
+ * Linje 16: For-løkke som itererer gjennom hver kamp
+ * Linje 17: Splitter hver kamp inn i en liste med data
  */
-var url = "data/kamperdata.html";
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url, true);
-xhr.responseType = 'document';
-xhr.send();
-xhr.onload = function(e) {
-    var doc = e.target.responseXML;
-    console.log("yolo");
-    var data = doc.getElementById('data');
-    console.log(data.children);
-};
 
+//START innlasting
+function lastInnFil(){
+    var fil = document.getElementById("fraFil");
+    var filInnhold = fil.contentWindow.document.body.children[0].innerHTML;
+    var kamper = filInnhold.split("\n");
+
+    for(var i = 0; i<kamper.length; i++){
+        var kamp = kamper[i].split(","); //
+        /**
+         * kamp[0] = startdato og tidspunkt
+         * kamp[1] = slutt
+         * kamp[2] = navn på turnering
+         * kamp[3] = url til turneringens nettside
+         * kamp[4] = Carlsens plassering hvis tilgjengelig (vil ikke være
+         * tilgjengelig hvis ikke kampen har vært enda, eller hvis
+         * resultat ikke er lagt inn enda).
+         * kamp[5] = Antall poeng hvis tilgjengelig
+         */
+        // SLUTT innlasting
+        // START kommende/resultatinnlegging
+        var slutt = kamp[0].split('-'); //[0] = år, [1] = måned, [2] = dag, [3] = time
+        for(var i = 0; i<slutt.length; i++){
+            slutt[i] = parseInt(slutt[i], 10); //Konverterer alle elementene i arrayet til integers
+        }
+        var sluttDate = new Date(slutt[0], slutt[1]-1, slutt[2], slutt[3]);
+        // -1 på måned fordi Date-objektet indekserer måneder fra null
+        var now = new Date();
+        if(sluttDate > now){
+            //sett inn under kommende kamper
+        }
+        else{
+            //sett inn under resultater
+        }
+    }
+}
