c76894bf0c9c4067f0dc8799aba6630e38c902fd
kollstrom <e.kollstrom@gmail.com>
2015-11-04 16:47:53 +0100

    kamper.txt-fix

--- scripts/resultater.js
+++ scripts/resultater.js
@@ -25,20 +25,24 @@ function lastInnFil(){
          * resultat ikke er lagt inn enda).
          * kamp[5] = Antall poeng hvis tilgjengelig
          */
-        // SLUTT innlasting
-        // START kommende/resultatinnlegging
+
+        var listElement = document.createElement('LI'); //Lager <li>-element
+        listElement.appendChild(document.createTextNode(kamp.join()));
+        // Til sammenligning av datoer:
         var slutt = kamp[0].split('-'); //[0] = år, [1] = måned, [2] = dag, [3] = time
-        for(var i = 0; i<slutt.length; i++){
-            slutt[i] = parseInt(slutt[i], 10); //Konverterer alle elementene i arrayet til integers
+        for(var x = 0; x<slutt.length; x++){
+            slutt[x] = parseInt(slutt[x], 10); //Konverterer alle elementene i arrayet til integers
         }
         var sluttDate = new Date(slutt[0], slutt[1]-1, slutt[2], slutt[3]);
         // -1 på måned fordi Date-objektet indekserer måneder fra null
         var now = new Date();
         if(sluttDate > now){
             //sett inn under kommende kamper
+            document.getElementById("kommende").appendChild(listElement);
         }
         else{
             //sett inn under resultater
+            document.getElementById("resultater").appendChild(listElement);
         }
     }
 }
