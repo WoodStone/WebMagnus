8d2b37d86da78dcab2f1efb163786b90e50e3c73
kollstrom <e.kollstrom@gmail.com>
2015-11-04 21:17:45 +0100

    Gjorde faneinnhold om til table

--- scripts/resultater.js
+++ scripts/resultater.js
@@ -21,10 +21,7 @@ function lastInnFil(){
          * kamp[5] = Antall poeng hvis tilgjengelig
          */
 
-        var listElement = document.createElement('LI'); //Lager <li>-element
         var start = kamp[0].substring(0, kamp[0].length-3);
-        //listElement.appendChild(document.createTextNode(kamp.join()));
-        listElement.appendChild(document.createTextNode(start));
         // Til sammenligning av datoer:
         var slutt = kamp[1].split('-'); //[0] = år, [1] = måned, [2] = dag, [3] = time
         for(var x = 0; x<slutt.length; x++){
@@ -33,13 +30,43 @@ function lastInnFil(){
         var sluttDate = new Date(slutt[0], slutt[1]-1, slutt[2], slutt[3]);
         // -1 på måned fordi Date-objektet indekserer måneder fra null
         var now = new Date();
+
+        var a = document.createElement('a');
+        var kampLink = document.createTextNode(kamp[2]);
+        a.appendChild(kampLink);
+        a.title = kamp[2];
+        a.href = kamp[3];
+        var parsed = [kamp[0].substring(0, kamp[0].length-3)]; //dato
+        parsed.push(a); // link
+
+
         if(sluttDate > now){
-            //sett inn under kommende kamper
-            document.getElementById("kommende").appendChild(listElement);
+            //legg til under kommende kamper
+            //document.getElementById("kommende").appendChild(a);
+            leggTilTurnering("kommende", parsed);
         }
         else{
-            //sett inn under resultater
-            document.getElementById("resultater").appendChild(listElement);
+            //legg til under resultater
+            //document.getElementById("resultater").appendChild(a);
+            parsed.push(kamp[4]); // Plassering
+            parsed.push(kamp[5]); // Antall poeng
+            leggTilTurnering("resultater", parsed);
+        }
+    }
+}
+
+function leggTilTurnering(id, parsed){
+    var table = document.getElementById(id);
+    var rad = table.insertRow(table.length);
+
+    for(y = 0; y<parsed.length; y++){
+        if(y !== 1){
+            var celle = rad.insertCell(rad.length);
+            celle.innerHTML = parsed[y];
+        }
+        else{
+            var c = rad.insertCell(rad.length);
+            c.appendChild(parsed[1]);
         }
     }
 }
